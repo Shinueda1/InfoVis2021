9441162bcf33d090781ea3112c7f36beb45ae491
shinnosuke <Shinueda1>
2021-04-14 14:46:29 +0900

    Initial commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -1,8 +1,16 @@
+Vec3 = function(x, y, z)
+{
+  this.x=x;
+  this.y=y;
+  this.z=z;
+}
+
+
 Vec3.prototype.add = function(v)
 {
-  this.x = v.x;
-  this.y = v.y;
-  this.z = v.z;
+  this.x += v.x;
+  this.y += v.y;
+  this.z += v.z;
   return this;
 }
 
