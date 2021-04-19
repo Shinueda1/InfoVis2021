9aaee17dd9d4056cf1c7fd56306f5a7a0d49a872
shinnosuke <Shinueda1>
2021-04-19 18:29:27 +0900

    Initial commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -1,20 +1,67 @@
-Vec3 = function(x, y, z)
+//Vec3 = function(x, y, z)
+//{
+//  this.x=x;
+//  this.y=y;
+//  this.z=z;
+//}
+
+//Vec3.prototype.add = function(v)
+//{
+//  this.x += v.x;
+//  this.y += v.y;
+//  this.z += v.z;
+//  return this;
+//}
+
+//Vec3.prototype.sum = function()
+//{
+//  return this.x + this.y + this.z;
+//}
+
+class Vec3
 {
-  this.x=x;
-  this.y=y;
-  this.z=z;
-}
+  constructor(x,y,z)
+  {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  min()
+  {
+    ver min = this.x;
+    if (min >= this.y){
+      min = this.y;
+    }
+    if(min >= this.z){
+      min = this.z;
+    }
+    return min;
+  }
 
 
-Vec3.prototype.add = function(v)
-{
-  this.x += v.x;
-  this.y += v.y;
-  this.z += v.z;
-  return this;
-}
+  max(){
+    ver max = this.x;
+    if (max <= this.y){
+      max = this.y;
+    }
+    if(max <= this.z){
+      max = this.z;
+    }
+    return max;
+  }
 
-Vec3.prototype.sum = function()
-{
-  return this.x + this.y + this.z;
+  mid(){
+    if(this.min() == this.max())
+    {
+      return this.min();
+    }
+    else
+    {
+      ver max = this.max;
+      ver min = this.min;
+      ver v  = this.filter(n => (n != max)&&(n != min));
+      return v;
+    }
+  }
 }
